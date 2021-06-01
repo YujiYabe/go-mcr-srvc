24a62e62a5554d73548e082bf537a2de07afad67
Yuji Yabe <yayuji@gmail.com>
2021-06-01 22:21:26 +0900

    wip

---
+++ backend/scripts/order/order.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"delivery"
+
+	grpc "google.golang.org/grpc"
+)
+
+func main() {
+	dial, err := grpc.Dial("localhost:3456", grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		return
+	}
+	defer dial.Close()
+
+	conn := delivery.NewDeliveryServiceClient(dial)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	var request = &delivery.DeliveryRequest{
+		Order: &delivery.Order{
+			Hamburger: &delivery.Hamburger{
+				Top: 1,
+			},
+		},
+	}
+
+	res, err := conn.DeliveryRPC(ctx, request)
+	if err != nil {
+		return
+	}
+	fmt.Println(" ============================== ")
+	fmt.Printf("%+v\n", res.String())
+	fmt.Println(" ============================== ")
+
+	return
+}
--- backend/scripts/order/readme.md
+++
@@ -1,7 +0,0 @@
-
-install prerequisites
-https://grpc.io/docs/languages/python/quickstart/
-
-python -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. ./delivery.proto
-
-
