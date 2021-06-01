a265020289d678b90fcd3c3d74e32f7018bd06f8
Yuji Yabe <yayuji@gmail.com>
2021-06-01 23:12:46 +0900

    wip

--- backend/scripts/order/order.go
+++ backend/scripts/order/order.go
@@ -13,6 +13,9 @@ import (
 func main() {
 	dial, err := grpc.Dial("localhost:3456", grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
+		fmt.Println(" ============================== ")
+		fmt.Printf("%+v\n", err)
+		fmt.Println(" ============================== ")
 		return
 	}
 	defer dial.Close()
@@ -24,19 +27,28 @@ func main() {
 
 	var request = &delivery.DeliveryRequest{
 		Order: &delivery.Order{
-			Hamburger: []*delivery.Hamburger{
-				{
-					Top:    1,
-					Cheese: 1,
-				},
-			},
+			Combo:     []*delivery.Combo{},
+			Hamburger: []*delivery.Hamburger{},
+			SideMenu:  []*delivery.SideMenu{},
+			Drink:     []*delivery.Drink{},
+			// Hamburger: []*delivery.Hamburger{
+			// 	{
+			// 		Top:    1,
+			// 		Cheese: 1,
+			// 	},
+			// },
 		},
 	}
 
 	res, err := conn.DeliveryRPC(ctx, request)
 	if err != nil {
+		fmt.Println(" ============================== ")
+		fmt.Printf("%+v\n", err)
+		fmt.Println(" ============================== ")
+
 		return
 	}
+
 	fmt.Println(" ============================== ")
 	fmt.Printf("%+v\n", res.String())
 	fmt.Println(" ============================== ")
