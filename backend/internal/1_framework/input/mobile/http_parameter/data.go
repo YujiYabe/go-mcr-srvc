05c213f2a3bf407d51ee76601d8ec07409d2fc32
YABEyuji <yayuji@gmail.com>
2024-12-24 09:53:57 +0900

    wip

---
+++ backend/internal/1_framework/input/mobile/http_parameter/data.go
@@ -0,0 +1,7 @@
+package http_parameter
+
+type HTTPParameter struct {
+	ID          int    `json:"status"`
+	Name        string `json:"name"`
+	MailAddress string `json:"mail_address"`
+}
--- backend/internal/1_framework/input/mobile/v1/person/get.go
+++ backend/internal/1_framework/input/mobile/v1/person/get.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/labstack/echo"
 
+	"backend/internal/1_framework/input/mobile/http_parameter"
 	"backend/internal/2_adapter/controller"
 	"backend/pkg"
 )
@@ -30,10 +31,20 @@ func get(
 		)
 	}
 
-	pkg.Logging(ctx, personList)
+	dataList := []http_parameter.HTTPParameter{}
+	for _, person := range personList {
+		dataList = append(
+			dataList,
+			http_parameter.HTTPParameter{
+				ID:          person.ID.Content.Value,
+				Name:        person.Name.Content.Value,
+				MailAddress: person.MailAddress.Content.Value,
+			},
+		)
+	}
 
 	return c.JSON(
 		http.StatusOK,
-		nil,
+		dataList,
 	)
 }
