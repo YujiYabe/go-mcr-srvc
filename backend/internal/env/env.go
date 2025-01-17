e279af4fee0bbded524a1778bcced2e59e1a2d63
YABEyuji <yayuji@gmail.com>
2025-01-17 21:18:02 +0900

    Adds environment-specific configuration handling
    Introduces logic to handle different configurations for LOCAL, DEV, and PROD environments.
    Integrates AWS Secrets Manager for fetching secrets in local environment using LocalStack.
    Improves configuration and environment management by dynamically setting config files based on the environment.
    
    Enhances maintainability by segregating environment-specific setup logic.

--- backend/internal/env/env.go
+++ backend/internal/env/env.go
@@ -1,8 +1,13 @@
 package env
 
 import (
+	"context"
 	"log"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
 	"github.com/spf13/viper"
 )
 
@@ -17,20 +22,32 @@ var (
 	GoEchoPort  string
 	GRPCPort    string
 	GRPCAddress string
-
-	TZ string
-
+	TZ          string
 	PostgresDSN string
 )
-
-var AlcoholIndex = 99
 
 func init() {
 	v := viper.New()
 	v.AutomaticEnv()
 	v.AddConfigPath("internal/env")
-	v.SetConfigName(".localenv")
 	v.SetConfigType("env")
+
+	switch v.GetString("ENV") {
+	case LOCAL:
+		v.SetConfigName(".localenv")
+		//local環境ではlocalstackを使う
+		localstack(*v)
+
+	case DEV:
+		v.SetConfigName(".devenv")
+
+	case PROD:
+		v.SetConfigName(".prodenv")
+
+	default:
+		log.Fatalf("failed to serve: invalid environment")
+	}
+
 	if err := v.ReadInConfig(); err != nil {
 		log.Println("== == == == == == == == == == ")
 		log.Printf("%#v\n", err)
@@ -53,3 +70,63 @@ func init() {
 		" sslmode=disable"
 
 }
+
+func localstack(viper.Viper) {
+	// customResolver := aws.EndpointResolverWithOptionsFunc(func(service, region string, options ...interface{}) (aws.Endpoint, error) {
+	// 	if service == secretsmanager.ServiceID {
+	// 		return aws.Endpoint{
+	// 			// URL: "http://localhost:4566/", // LocalStack のエンドポイント
+	// 			URL: "http://localstack:4566", // LocalStack のエンドポイント
+	// 		}, nil
+	// 	}
+	// 	return aws.Endpoint{}, &aws.EndpointNotFoundError{}
+	// })
+
+	creds := credentials.NewStaticCredentialsProvider("test", "test", "")
+
+	secretName := "my-local-secret"
+	region := "ap-northeast-1"
+
+	config, err := config.LoadDefaultConfig(
+		context.TODO(),
+		config.WithCredentialsProvider(creds),
+		config.WithRegion(region),
+		config.WithBaseEndpoint("http://localstack:4566"),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create Secrets Manager client
+	svc := secretsmanager.NewFromConfig(config)
+
+	input := &secretsmanager.GetSecretValueInput{
+		SecretId: aws.String(secretName),
+	}
+
+	result, err := svc.GetSecretValue(context.TODO(), input)
+	if err != nil {
+
+		log.Fatal(err.Error())
+	}
+
+	var secretString string = aws.ToString(result.SecretString)
+	log.Println("== == == == == == == == == == ")
+	log.Printf("%#v\n", secretString)
+	log.Println("== == == == == == == == == == ")
+
+}
+
+// Secrets Managerからシークレットを取得する関数
+// func getSecretValue(client *secretsmanager.Client, secretName string) (string, error) {
+// 	input := &secretsmanager.GetSecretValueInput{
+// 		SecretId: aws.String(secretName),
+// 	}
+
+// 	result, err := client.GetSecretValue(context.TODO(), input)
+// 	if err != nil {
+// 		return "", err
+// 	}
+
+// 	return aws.ToString(result.SecretString), nil
+// }
